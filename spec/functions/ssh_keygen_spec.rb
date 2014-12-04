3a2401ad3f448f8692bd91442504068a5e40b6ce
Patrick Schoenfeld <patrick.schoenfeld@credativ.de>
2014-12-04 01:57:17 +0100

    Fix exception test

--- spec/functions/ssh_keygen_spec.rb
+++ spec/functions/ssh_keygen_spec.rb
@@ -1,7 +1,11 @@
 require 'spec_helper'
 
 describe 'ssh_keygen' do
-    describe 'requires arguments' do
-        it { should run.with_params().and_raise_error("/request argument required/") }
-    end
+	context "test for expected exceptions" do
+		it "raises expected error with no arguments" do
+			should run.with_params().and_raise_error(Puppet::ParseError, /argument must be a Hash/)
+		end
+
+
+	end
 end
