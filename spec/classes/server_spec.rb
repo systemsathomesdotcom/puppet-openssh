5ee79983fc1731b5c42b48b346495c37db43d5cb
Patrick Schoenfeld <patrick.schoenfeld@credativ.de>
2014-12-07 14:05:47 +0100

    Extend tests

--- spec/classes/server_spec.rb
+++ spec/classes/server_spec.rb
@@ -8,7 +8,27 @@ describe 'ssh::server' do
 	context "basic tests" do
 		it { should contain_class('ssh::server') }
 		it { should contain_package('openssh-server').with( :ensure => 'present' ) }
-	end
+	    it { should contain_service('ssh').with(
+                :ensure => true,
+                :enable => true
+            )}
+        it { should contain_file('/etc/ssh/sshd_config').with(
+                :owner  => 'root',
+                :group  => 'root',
+                :mode   => '0644',
+           )}
+    end
+
+    context "RedHat-specific differences" do
+        let (:facts) {{
+            :osfamily   => 'RedHat'
+        }}
+
+        it { should contain_service('sshd').with(
+                :ensure => true,
+                :enable => true
+            )}
+    end
 
 	context "absence test" do
 		let (:params) { { :ensure => 'absent' } }
